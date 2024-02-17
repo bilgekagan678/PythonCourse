75df5b2f565633ca68794a6553479d5e3567a3d4
Bilge Kağan Toprak <bilgekagantoprak@gmail.com>
2024-02-17 14:44:15 +0300

    UI generated

---
+++ secretNotes/secretNotes.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+window = tk.Tk()
+window.title("Secret Notes")
+window.config(padx=30, pady=30)
+
+# UI
+image = Image.open("top_secret.png")
+pixels_x, pixels_y = tuple([int(0.1 * x) for x in image.size])
+img = ImageTk.PhotoImage(image.resize((pixels_x, pixels_y)))
+label_img = tk.Label(window, image=img)
+label_img.image = img
+label_img.pack()
+
+title_label = tk.Label(window, text="Enter your title")
+title_label.pack()
+title_entry = tk.Entry(window, width=65)
+title_entry.pack()
+
+message_label = tk.Label(window, text="Enter your message")
+message_label.pack()
+message_text = tk.Text(window, width=50, height=20)
+message_text.pack()
+
+key_label = tk.Label(window, text="Enter your key")
+key_label.pack()
+key_entry = tk.Entry(window, width=65)
+key_entry.pack()
+
+save_encrypt_btn = tk.Button(text="Save & Encrypt")
+save_encrypt_btn.pack()
+
+decrypt_btn = tk.Button(text="Decrypt")
+decrypt_btn.pack()
+
+window.mainloop()
