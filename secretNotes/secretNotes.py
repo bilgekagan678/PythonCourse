90a75ddffd0ca9e07cc7d991b8c3e47afbc64085
Bilge Kağan Toprak <bilgekagantoprak@gmail.com>
2024-02-17 14:50:26 +0300

    UI revision

--- secretNotes/secretNotes.py
+++ secretNotes/secretNotes.py
@@ -13,25 +13,25 @@ label_img = tk.Label(window, image=img)
 label_img.image = img
 label_img.pack()
 
-title_label = tk.Label(window, text="Enter your title")
+title_label = tk.Label(window, text="Enter Your Title", font=("Arial", 10, "bold"))
 title_label.pack()
 title_entry = tk.Entry(window, width=65)
 title_entry.pack()
 
-message_label = tk.Label(window, text="Enter your message")
+message_label = tk.Label(window, text="Enter Your Message", font=("Arial", 10, "bold"))
 message_label.pack()
 message_text = tk.Text(window, width=50, height=20)
 message_text.pack()
 
-key_label = tk.Label(window, text="Enter your key")
+key_label = tk.Label(window, text="Enter Your Key", font=("Arial", 10, "bold"))
 key_label.pack()
 key_entry = tk.Entry(window, width=65)
 key_entry.pack()
 
-save_encrypt_btn = tk.Button(text="Save & Encrypt")
+save_encrypt_btn = tk.Button(text="Save & Encrypt", font=("Arial", 8, "bold"))
 save_encrypt_btn.pack()
 
-decrypt_btn = tk.Button(text="Decrypt")
+decrypt_btn = tk.Button(text="Decrypt", font=("Arial", 8, "bold"))
 decrypt_btn.pack()
 
 window.mainloop()
