c275b11cea3fd8d6d6e4275cc33dd78673df7cf8
Bilge Kağan Toprak <bilgekagantoprak@gmail.com>
2024-02-24 11:57:01 +0300

    End-to-End Test HW done.

--- EndToEndTestHW/endToEnd.py
+++ EndToEndTestHW/endToEnd.py
@@ -1,11 +1,8 @@
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
 
 
 class TestUnicornWebsite(unittest.TestCase):
@@ -16,8 +13,21 @@ class TestUnicornWebsite(unittest.TestCase):
         driver = self.driver
         driver.get("http://unicornitems.com/my-account/")
 
+        elemUser = driver.find_element(By.NAME, "username")
+        elemUser.send_keys("bilgekagan")
+
+        elemPass = driver.find_element(By.NAME, "password")
+        elemPass.send_keys("12345")
+
+        elemLogin = driver.find_element(By.NAME, "login")
+        elemLogin.click()
+
+        assert "INCORRECT USERNAME OR PASSWORD." not in driver.page_source
+        # assert "INCORRECT USERNAME OR PASSWORD." in driver.page_source
+
     def tearDown(self):
         self.driver.close()
+
 
 if __name__ == '__main__':
     unittest.main()
