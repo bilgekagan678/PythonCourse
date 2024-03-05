7c7cc52655f7a3de9884d2e310f34a95d3f53a8a
Bilge Kağan Toprak <bilgekagantoprak@gmail.com>
2024-03-06 02:17:01 +0300

    Subtractive Roman numbers implemented

--- RomenNumerals/romanNumeralConverter.py
+++ RomenNumerals/romanNumeralConverter.py
@@ -14,10 +14,15 @@ int_num = 0
 
 for i in range(len(roman_num)-1):
     if roman_num[i] in roman_dict.keys():
-        int_num += roman_dict.get((roman_num[i]))
+        if roman_dict.get(roman_num[i+1]) > roman_dict.get(roman_num[i]):
+            int_num -= roman_dict.get(roman_num[i])
+        else:
+            int_num += roman_dict.get((roman_num[i]))
     else:
         print("Please enter a valid number!")
         int_num = ''
 
+if roman_num[-1] in roman_dict.keys():
+    int_num += roman_dict[roman_num[-1]]
 
 print(int_num)
