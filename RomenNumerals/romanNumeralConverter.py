a72b503066a58fc3454416c7e847f753483c2a8a
Bilge Kağan Toprak <bilgekagantoprak@gmail.com>
2024-03-06 02:13:08 +0300

    Additive Roman numbers implemented

--- RomenNumerals/romanNumeralConverter.py
+++ RomenNumerals/romanNumeralConverter.py
@@ -10,5 +10,14 @@ roman_dict = {
 
 roman_num = input("Please enter Roman Number between 1 and 4999: ").upper()
 
-if roman_num in roman_dict.keys():
-    print(roman_dict.get(roman_num))
+int_num = 0
+
+for i in range(len(roman_num)-1):
+    if roman_num[i] in roman_dict.keys():
+        int_num += roman_dict.get((roman_num[i]))
+    else:
+        print("Please enter a valid number!")
+        int_num = ''
+
+
+print(int_num)
