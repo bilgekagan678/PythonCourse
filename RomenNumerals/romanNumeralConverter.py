1e22dd174556041ff33e6ac75576ca08f7eefa52
Bilge Kağan Toprak <bilgekagantoprak@gmail.com>
2024-03-07 20:54:08 +0300

    Additive and Subtractive Roman numbers revised

--- RomenNumerals/romanNumeralConverter.py
+++ RomenNumerals/romanNumeralConverter.py
@@ -1,28 +1,47 @@
-roman_dict = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000
-}
+def roman_to_int(roman_num):
+    roman_dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
 
-roman_num = input("Please enter Roman Number between 1 and 4999: ").upper()
+    invalid_patterns = ["IIII", "XXXX", "CCCC", "MMMMM"]
 
-int_num = 0
+    for pattern in invalid_patterns:
+        if pattern in roman_num:
+            print("Invalid Roman numeral pattern detected.")
+            return None
 
-for i in range(len(roman_num)-1):
-    if roman_num[i] in roman_dict.keys():
-        if roman_dict.get(roman_num[i+1]) > roman_dict.get(roman_num[i]):
-            int_num -= roman_dict.get(roman_num[i])
+    int_num = 0
+    prev_num = 0
+
+    for i in range(len(roman_num) - 1, -1, -1):
+        if roman_num[i] in roman_dict.keys():
+            curr_num = roman_dict[roman_num[i]]
+            if curr_num < prev_num:
+                int_num -= curr_num
+            else:
+                int_num += curr_num
+            prev_num = curr_num
         else:
-            int_num += roman_dict.get((roman_num[i]))
-    else:
+            print("Please enter a valid number!")
+            return None
+
+    return int_num
+
+
+def main():
+    roman = input("Please enter Roman Number between 1 and 4999: ")
+    if not roman.isalpha():
         print("Please enter a valid number!")
-        int_num = ''
+    integer = roman_to_int(roman.upper())
+    if integer:
+        print(f"The Arabic integer for {roman.upper()} is {integer}")
 
-if roman_num[-1] in roman_dict.keys():
-    int_num += roman_dict[roman_num[-1]]
 
-print(int_num)
+if __name__ == "__main__":
+    main()
